d5e843327e5a8d05de9055bfc7b6b5c633823143
Nplite <76342ns@gmail.com>
2023-03-04 21:58:56 +0530

    data validation done

--- Travel_Package_prediction/entity/config_entity.py
+++ Travel_Package_prediction/entity/config_entity.py
@@ -39,4 +39,12 @@ class DataIngestionConfig:
         try:
             return self.__dict__
         except Exception  as e:
-            raise Travel_Exception(e,sys)          
+            raise Travel_Exception(e,sys)   
+
+class DataValidationConfig:
+    def __init__(self,training_pipline_config: TrainingPipelineConfig):
+        self.data_validation_dir = os.path.join(training_pipline_config.artifact_dir, "data_validation")
+        self.report_file_path = os.path.join(self.data_validation_dir,"report.yaml")
+        self.missing_threshold:float = 0.3
+        self.base_file_path = os.path.join("TourismData.csv")
+
