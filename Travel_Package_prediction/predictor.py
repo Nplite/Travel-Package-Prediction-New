6118c3fa47dd766ee1abc0c7ee9f4ce23a33a65e
Nplite <76342ns@gmail.com>
2023-03-29 19:05:51 +0530

    predictor.py file updated

--- Travel_Package_prediction/predictor.py
+++ Travel_Package_prediction/predictor.py
@@ -1,5 +1,5 @@
 import os, sys
-import Optional
+from typing import Optional
 from Travel_Package_prediction.entity.config_entity import TRANSFORMER_OBJECT_FILE_NAME,MODEL_FILE_NAME, TARGET_ENCODER_OBJECT_FILE_NAME
 # Folder (new model saved)
 # Comparision of new model with old model
@@ -52,8 +52,8 @@ class ModelResolver:
             latest_dir = self.get_latest_dir_path()
             if latest_dir is None:
                 raise Exception(f"Transformed data is not available")
+            return os.path.join(latest_dir, self.transformer_dir_name, TRANSFORMER_OBJECT_FILE_NAME)
             
-            return os.path.join(latest_dir, self.target_encoder_dir_name, TARGET_ENCODER_OBJECT_FILE_NAME)
         except Exception as e:
             raise e
 
@@ -65,7 +65,7 @@ class ModelResolver:
             if latest_dir is None:
                 raise Exception(f"Target encoder data is not available")
             
-            return os.path.join(latest_dir, self.transformer_dir_name, TRANSFORMER_OBJECT_FILE_NAME)
+            return os.path.join(latest_dir, self.target_encoder_dir_name, TARGET_ENCODER_OBJECT_FILE_NAME)
         except Exception as e:
             raise e
 
@@ -76,7 +76,7 @@ class ModelResolver:
             if latest_dir is None:
                 return os.path.join(self.model_registry, f"{0}")
             latest_dir_num = int(os.path.basename(self.get_latest_dir_path()))
-            return os.path.join(self.model_registry, f"{latest_dir_num}")
+            return os.path.join(self.model_registry, f"{latest_dir_num + 1}")
         except Exception as e:
             raise e
         
@@ -96,8 +96,6 @@ class ModelResolver:
         except Exception as e:
             raise e
         
-
-
 
 
     def get_latest_save_target_encoder_path(self):
