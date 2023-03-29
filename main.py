07bb2ca8d3bfd6bd52449963d1db4bbdd5209237
Nplite <76342ns@gmail.com>
2023-03-29 17:01:43 +0530

    main.py file updated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from Travel_Package_prediction.components.model_trainer import ModelTrainer
 from Travel_Package_prediction.entity.artifact_entity import DataIngestionArtifact
 from Travel_Package_prediction.logger import logging
 from Travel_Package_prediction.exception import Travel_Exception
@@ -29,16 +30,22 @@ if __name__=="__main__":
      # Data Validation
         data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
         data_validation = DataValidation(data_validation_config=data_validation_config,
-                                          data_ingestion_artifact = data_ingestion_artifact)
+                                        data_ingestion_artifact = data_ingestion_artifact)
         
         data_validation_artifact = data_validation.initiate_data_validation()
 
-      #Data Transformation
+      # Data Transformation
 
         data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
         data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
                                                    data_ingestion_artifact = data_ingestion_artifact)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
+
+      # Model Trainer
+
+        model_trainer_config = config_entity.ModelTrainingConfig(training_pipeline_config=training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config= model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
 
     except Exception as e:
         print(e)
