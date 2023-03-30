be6cde2debb8c3e4e24808377c254518c3d7a084
Nplite <76342ns@gmail.com>
2023-03-30 15:26:55 +0530

    model evaluation done

--- Travel_Package_prediction/components/model_evaluation.py
+++ Travel_Package_prediction/components/model_evaluation.py
@@ -2,6 +2,8 @@ from Travel_Package_prediction.entity import artifact_entity,config_entity
 from Travel_Package_prediction.exception import Travel_Exception
 from Travel_Package_prediction.logger import logging
 from Travel_Package_prediction.predictor import ModelResolver
+from Travel_Package_prediction.utils import load_object
+from Travel_Package_prediction.config import TARGET_COLUMN
 from typing import Optional
 import os,sys 
 from sklearn.pipeline import Pipeline
@@ -41,8 +43,74 @@ class ModelEvaluation:
                 logging.info(f"Model evaluation artifact: {model_evaluation_artifact}")
 
                 return model_evaluation_artifact
+            
+
+
+            # find location of previous model
+            transformer_path = self.model_resolver.get_latest_transformer_path()
+            model_path = self.model_resolver.get_latest_save_model_path()
+            target_encoder_path = self.model_resolver.get_latest_target_encoder_path()
+
+            # previous model
+            transformer = load_object(File_path=transformer_path)
+            model = load_object(file_path = model_path)
+            target_encoder = load_object(file_path = target_encoder_path)
+
+            # New model
+            current_transformer = load_object(file_path = self.data_transformation_artifact.transform_object_path)
+            current_model = load_object(file_path = self.model_trainer_artifact.model_path)
+            current_target_encoder = load_object(file_path=self.data_transformation_artifact.target_encoder_path)
+
+
+            test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+            target_df = test_df[TARGET_COLUMN]
+            y_true = target_df
+
+
+            input_features_name = list(transformer.feature_name_in_)
+            for i in input_features_name:
+                if test_df[i].dtypes=="object":
+                    test_df[i] = target_encoder.fit_transform(test_df[i])
+
+            input_arr = transformer.transform(test_df[input_features_name])
+            y_pred = model.predict(input_arr)
+
+
+
+            # comparision between new model and old model
+            previous_model_score = r2_score(y_true= y_true,y_pred = y_pred)
+
+
+            # accuracy of current trained model
+            input_features_name = list(current_transformer.feature_name_in_)
+            input_arr = current_transformer.transform(test_df[input_features_name])
+            y_pred = current_model.predict(input_arr)
+            y_true = target_df
+
+            current_model_score = r2_score(y_true = y_true, y_pred = y_pred)
+
+
+            # Final camparision between both model
+            if current_model_score < previous_model_score :
+                logging.info(f"current trained model is not better than previous model")
+                raise Exception("current model is not better than previous model")
+            
+
+            mode_evaluation_artifact = artifact_entity.ModelEvaluationArtifact(is_model_accepted=True, 
+            improved_accuracy= current_model_score-previous_model_score)
+            
+
+            return model_evaluation_artifact    
+
         except Exception as e:
             raise Travel_Exception(e,sys)
+        
+
+
+
+# we can store output in cloud platform
+# aws ->s3 bucket
+# Database ->Model pusher
 
 
 
